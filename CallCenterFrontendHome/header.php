cd6489c9fffd55356b495f7b9de12d89670d3021
marsssi <marsix2000@gmail.com>
2022-01-28 15:51:04 +0100

    Callcenter Homepage Frontend

---
+++ CallCenterFrontendHome/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+      crossorigin="anonymous";
+    />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <!-- FILE I CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <title>CallCenter</title>
+  </head>
+
+  <!-- NAVBAR -->
+  <nav id="navbar-example2" class="navbar navbar-light bg-light px-3">
+    <a class="navbar-brand" href="home.php"> <img src="photos\logo.jpg" style=" height: 60px;"></a>
+    <a class="navbar-brand" href="home.php" style="margin-left: -30%; color: rgb(24, 113, 230);"> <bold>𝐂𝐨𝐝𝐞𝐫𝐂𝐚𝐥𝐥</bold></a>
+    <ul class="nav nav-pills">
+      <li class="nav-item"><a class="nav-link" href="home.php">𝐇𝐨𝐦𝐞</a></li>
+      <li class="nav-item"><a class="nav-link" href="about.php">𝐀𝐛𝐨𝐮𝐭 𝐮𝐬</a></li>
+      <li class="nav-item"><a class="nav-link" href="blog.php">𝐁𝐥𝐨𝐠</a></li>
+      <li class="nav-item"><a class="nav-link" href="jobs.php">𝐉𝐨𝐛𝐬</a></li>
+      <li class="nav-item"><a class="nav-link" href="contact.php">𝐂𝐨𝐧𝐭𝐚𝐜𝐭</a></li>
+      <li class="nav-item dropdown">
+        
+          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            𝐋𝐨𝐠 𝐢𝐧 
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="admin.php">Administrator</a>
+              <a class="dropdown-item" href="manager.php">Manager</a>
+              <a class="dropdown-item" href="operator.php">Operator</a>
+            </div>   
+      </li> 
+  </nav>
+
+  <body>
